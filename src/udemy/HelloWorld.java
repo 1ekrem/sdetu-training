d253b7c08198cd0147db1f375096b7b6540517b5
Tim <erikersay@gmail.com>
2018-03-19 20:52:06 -0400

    Modified HelloWorld app

---
+++ src/udemy/HelloWorld.java
@@ -0,0 +1,10 @@
+package udemy;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("I'm adding code to my project");
+	}
+
+}
